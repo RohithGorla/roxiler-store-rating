05ef8c1446967667ed30efdfe9ec496d2f9e06ac
Rohith Gorla <129950040+RohithGorla@users.noreply.github.com>
2026-01-13 12:29:18 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,24 +1,19 @@
-const express = require("express")
-const cors = require("cors")
-require("dotenv").config()
+const express = require("express");
+const cors = require("cors");
 
-const authRoutes = require("./routes/authRoutes")
-const adminRoutes = require("./routes/adminRoutes")
-const userRoutes = require("./routes/userRoutes")
-const ownerRoutes = require("./routes/ownerRoutes")
+const app = express();
 
-const app = express()
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
-app.use("/api/auth", authRoutes)
-app.use("/api/admin", adminRoutes)
-app.use("/api/user", userRoutes)
-app.use("/api/owner", ownerRoutes)
+// routes
+app.use("/api/auth", require("./routes/authRoutes"));
+app.use("/api/admin", require("./routes/adminRoutes"));
+app.use("/api/user", require("./routes/userRoutes"));
+app.use("/api/owner", require("./routes/ownerRoutes"));
 
-aconst PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
-
